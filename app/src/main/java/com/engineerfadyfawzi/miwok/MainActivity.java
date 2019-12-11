9d4749f4991b3a893a9c6c61ae349748d6ee6fa4
Fady Fawzi <Engineer.FadyFawzi@gmail.com>
2019-12-11 20:44:59 +0200

    1.2 Send explicit intent to open the NumbersActivity

--- app/src/main/java/com/engineerfadyfawzi/miwok/MainActivity.java
+++ app/src/main/java/com/engineerfadyfawzi/miwok/MainActivity.java
@@ -2,7 +2,10 @@ package com.engineerfadyfawzi.miwok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +15,11 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+    }
+    
+    public void openNumbersList( View view )
+    {
+        Intent intent = new Intent( this, NumbersActivity.class );
+        startActivity( intent );
     }
 }
