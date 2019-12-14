3b0729dcb40f98c6ac2d5ecda5521d30e814bf51
Fady Fawzi <Engineer.FadyFawzi@gmail.com>
2019-12-14 11:02:19 +0200

    1.8 Switch to ArrayList of words in NumbersActivity

--- app/src/main/java/com/engineerfadyfawzi/miwok/NumbersActivity.java
+++ app/src/main/java/com/engineerfadyfawzi/miwok/NumbersActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity
 {
     
@@ -14,30 +16,29 @@ public class NumbersActivity extends AppCompatActivity
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_numbers );
         
-        // Create an array of words
-        String[] words = new String[ 10 ];
+        // create a list fo words
+        ArrayList< String > words = new ArrayList< String >();
+        words.add( "one" );
+        words.add( "two" );
+        words.add( "three" );
+        words.add( "four" );
+        words.add( "five" );
+        words.add( "six" );
+        words.add( "seven" );
+        words.add( "eight" );
+        words.add( "nine" );
+        words.add( "ten" );
         
-        words[ 0 ] = "one";
-        words[ 1 ] = "two";
-        words[ 2 ] = "three";
-        words[ 3 ] = "four";
-        words[ 4 ] = "five";
-        words[ 5 ] = "six";
-        words[ 6 ] = "seven";
-        words[ 7 ] = "eight";
-        words[ 8 ] = "nine";
-        words[ 9 ] = "ten";
-        
-        // Verify the contents of the array by printing out each array element to the logs
-        Log.v( "NumbersActivity", "Word at index 0: " + words[ 0 ] );
-        Log.v( "NumbersActivity", "Word at index 1: " + words[ 1 ] );
-        Log.v( "NumbersActivity", "Word at index 2: " + words[ 2 ] );
-        Log.v( "NumbersActivity", "Word at index 3: " + words[ 3 ] );
-        Log.v( "NumbersActivity", "Word at index 4: " + words[ 4 ] );
-        Log.v( "NumbersActivity", "Word at index 5: " + words[ 5 ] );
-        Log.v( "NumbersActivity", "Word at index 6: " + words[ 6 ] );
-        Log.v( "NumbersActivity", "Word at index 7: " + words[ 7 ] );
-        Log.v( "NumbersActivity", "Word at index 8: " + words[ 8 ] );
-        Log.v( "NumbersActivity", "Word at index 9: " + words[ 9 ] );
+        // Verify the contents of the list by printing out each array element to the logs
+        Log.v( "NumbersActivity", "Word at index 0: " + words.get( 0 ) );
+        Log.v( "NumbersActivity", "Word at index 1: " + words.get( 1 ) );
+        Log.v( "NumbersActivity", "Word at index 2: " + words.get( 2 ) );
+        Log.v( "NumbersActivity", "Word at index 3: " + words.get( 3 ) );
+        Log.v( "NumbersActivity", "Word at index 4: " + words.get( 4 ) );
+        Log.v( "NumbersActivity", "Word at index 5: " + words.get( 5 ) );
+        Log.v( "NumbersActivity", "Word at index 6: " + words.get( 6 ) );
+        Log.v( "NumbersActivity", "Word at index 7: " + words.get( 7 ) );
+        Log.v( "NumbersActivity", "Word at index 8: " + words.get( 8 ) );
+        Log.v( "NumbersActivity", "Word at index 9: " + words.get( 9 ) );
     }
 }
