93549d9adfc3d3b51c37dfb4962e1787e17b206e
Fady Fawzi <Engineer.FadyFawzi@gmail.com>
2019-12-31 18:51:13 +0200

    1.21 Don't show an image for phrases

--- app/src/main/java/com/engineerfadyfawzi/miwok/Word.java
+++ app/src/main/java/com/engineerfadyfawzi/miwok/Word.java
@@ -19,7 +19,12 @@ public class Word
     /**
      * Image resource ID for the word
      */
-    private int mImageResourceId;
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+    
+    /**
+     * Constant value that represents no image was provided for this word
+     */
+    private static final int NO_IMAGE_PROVIDED = -1;
     
     /**
      * Create a new Word object.
@@ -77,6 +82,16 @@ public class Word
     public int getImageResourceId()
     {
         return mImageResourceId;
+    }
+    
+    /**
+     * Returns whether or not there is an image for this word.
+     *
+     * @return
+     */
+    public boolean hasImage()
+    {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
     }
     
     // So we could of added two setter methods to allow the two strings to be changed
--- app/src/main/java/com/engineerfadyfawzi/miwok/WordAdapter.java
+++ app/src/main/java/com/engineerfadyfawzi/miwok/WordAdapter.java
@@ -70,11 +70,25 @@ public class WordAdapter extends ArrayAdapter< Word >
         defaultTranslationTextView.setText( currentWord.getDefaultTranslation() );
         
         // Find the ImageView in the list_item.xml layout with the ID image.
-        ImageView image = listItemView.findViewById( R.id.image );
+        ImageView imageView = listItemView.findViewById( R.id.image );
         
-        // Get the image resource ID from the currentWord object and
-        // Set the ImageView to the image resource specified in the current Word
-        image.setImageResource( currentWord.getImageResourceId() );
+        // Check if an image is provided for this Word or not
+        if ( currentWord.hasImage() )
+        {
+            // If an image is available, display the provided image based on the resource ID
+            
+            // Get the image resource ID from the currentWord object and
+            // Set the ImageView to the image resource specified in the current Word
+            imageView.setImageResource( currentWord.getImageResourceId() );
+            
+            // make sure the view is visible
+            // imageView.setVisibility( View.VISIBLE );
+        }
+        else
+        {
+            // Otherwise hide the ImageView (set visibility to GONE)
+            imageView.setVisibility( View.GONE );
+        }
         
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView.
