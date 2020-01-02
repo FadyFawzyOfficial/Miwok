7774aec576ea2be0bc7d5337e892e353387c92c9
Fady Fawzi <Engineer.FadyFawzi@gmail.com>
2020-01-02 20:03:44 +0200

    1.24 Play same audio file for all words in NumbersActivity

--- app/src/main/java/com/engineerfadyfawzi/miwok/NumbersActivity.java
+++ app/src/main/java/com/engineerfadyfawzi/miwok/NumbersActivity.java
@@ -2,13 +2,17 @@ package com.engineerfadyfawzi.miwok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity
 {
+    private MediaPlayer mediaPlayer;
     
     @Override
     protected void onCreate( Bundle savedInstanceState )
@@ -41,5 +45,15 @@ public class NumbersActivity extends AppCompatActivity
         // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
         // {@link ListView} will display list items for each {@link Word} in the list of words.
         listView.setAdapter( wordsAdapter );
+        
+        listView.setOnItemClickListener( new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick( AdapterView< ? > adapterView, View view, int i, long l )
+            {
+                mediaPlayer = MediaPlayer.create( NumbersActivity.this, R.raw.number_one );
+                mediaPlayer.start();
+            }
+        } );
     }
 }
