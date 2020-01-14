bccfe9968b94e6a8f710e580ab13d17f075684a6
Fady Fawzi <Engineer.FadyFawzi@gmail.com>
2020-01-14 21:07:58 +0200

    1.35 Add ViewPager in MainActivity to swipe between 4 lists of words

---
+++ app/src/main/java/com/engineerfadyfawzi/miwok/CategoryAdapter.java
@@ -0,0 +1,54 @@
+package com.engineerfadyfawzi.miwok;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+/**
+ * {@link CategoryAdapter} is a {@link FragmentPagerAdapter} that can provide the layout for
+ * each list item based on a data source which is a list of {@link Word} objects.
+ */
+public class CategoryAdapter extends FragmentPagerAdapter
+{
+    /**
+     * Create a new {@link CategoryAdapter} object.
+     *
+     * @param fm is the fragment manager that will keep each fragment's state in the adapter
+     *           across swipes.
+     */
+    public CategoryAdapter( FragmentManager fm )
+    {
+        super( fm );
+    }
+    
+    /**
+     * Return the {@link Fragment} that should be displayed for the given page number.
+     */
+    @Override
+    public Fragment getItem( int position )
+    {
+        switch ( position )
+        {
+            case 0:
+                return new NumbersFragment();
+            
+            case 1:
+                return new FamilyFragment();
+            
+            case 2:
+                return new ColorsFragment();
+            
+            default:
+                return new PhrasesFragment();
+        }
+    }
+    
+    /**
+     * Return the total number of pages.
+     */
+    @Override
+    public int getCount()
+    {
+        return 4;
+    }
+}
