27a30d4564d8f48303504e5cc04e5bac62e1b64a
Fady Fawzi <Engineer.FadyFawzi@gmail.com>
2020-01-15 21:30:50 +0200

    1.36 Add tabs to ViewPager

--- app/src/main/java/com/engineerfadyfawzi/miwok/MainActivity.java
+++ app/src/main/java/com/engineerfadyfawzi/miwok/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity
 {
     @Override
@@ -19,9 +21,19 @@ public class MainActivity extends AppCompatActivity
         ViewPager viewPager = findViewById( R.id.view_pager );
         
         // Create an adapter that knows which fragment should be shown on each page
-        CategoryAdapter categoryAdapter = new CategoryAdapter( getSupportFragmentManager() );
+        CategoryAdapter categoryAdapter = new CategoryAdapter( this, getSupportFragmentManager() );
         
         // set the adapter onto the view pager
         viewPager.setAdapter( categoryAdapter );
+        
+        // Find the tab layout that shows the tabs
+        TabLayout tabLayout = findViewById( R.id.tab_layout );
+        
+        // Connect the tab layout with the view pager. This will
+        //      1. Update the tab layout when the view pager is swiped
+        //      2. Update the view pager when a tab is selected
+        //      3. Set the tab layout's tab names with the view pager's adapter's
+        //          by calling onPageTitle()
+        tabLayout.setupWithViewPager( viewPager );
     }
 }
